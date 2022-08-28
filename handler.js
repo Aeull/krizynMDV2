6adb7702cb1dcfe524e6149efb6d8fc9cea4bc13
Aeull <110038479+Aeull@users.noreply.github.com>
2022-08-28 10:22:07 +0800

    Update handler.js

--- handler.js
+++ handler.js
@@ -710,7 +710,7 @@ module.exports = {
                         } finally {
                             text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Yah,si Beban Masuk Grup @user').replace('@subject', groupMetadata.subject).replace('@desc', groupMetadata.desc.toString()) :
                                 (chat.sBye || this.bye || conn.bye || 'Sip, Beban Berkurang @user!')).replace('@user', '@' + user.split('@')[0])
-                                this.sendButtonImg(id, pp, text, "AdamOfc", "Menu", ".menu", null)
+                                this.sendButtonImg(id, pp, text, "𝘼𝙧𝙪𝙡𝙡𝘽𝙤𝙩𝙯", "Menu", ".menu", null)
                                 }
                     }
                 }
